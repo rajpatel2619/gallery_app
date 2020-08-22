4cddc4b2a1b07847206847ce9eac91d7e0b40cf0
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2020-08-22 17:53:29 +0530

    Update unsplash.js

--- api/unsplash.js
+++ api/unsplash.js
@@ -4,6 +4,6 @@ import axios from 'axios';
 export default axios.create({
 baseURL:'https://api.unsplash.com',
 headers:{
-    Authorization: 'Client-ID 9ZHclwnOGKASijtsrcvDUiH17Bmhie1N6Il_LlmX-Ng'
+    Authorization: ''
 }
 });
